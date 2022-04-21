0aa1ee4c8777a8d9f13a85c07ca97215ed5d3078
TravisAlstrand <travisalstrand@gmail.com>
2022-04-21 10:15:24 -0700

    Commit 7
     Issues needing help.

---
+++ app.js
@@ -0,0 +1,28 @@
+// variables
+const express = require('express');
+// const data = require('data.json');
+const app = express();
+
+// view engine setup
+app.set('view engine', 'pug');
+
+/* middleware for accessing the req.body */
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// static middleware setup
+app.use('/static', express.static('public'));
+
+// home page route
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// route to about page
+app.get('/about', (req, res) => {
+    req.redirect('/about');
+});
+
+app.listen(3000, () => {
+    console.log('The app is listening on port 3000');
+});
