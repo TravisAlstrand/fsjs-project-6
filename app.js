fbd66fad6b6a640b97367f8ec4f0f3f09c3d22d0
TravisAlstrand <travisalstrand@gmail.com>
2022-04-23 09:48:03 -0700

    Commit 9
    Routes, error handlers added. Started pug edits.

--- app.js
+++ app.js
@@ -15,14 +15,52 @@ app.use('/static', express.static('public'));
 
 // home page route
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', data.projects);
 });
 
 // route to about page
 app.get('/about', (req, res) => {
-    req.redirect('/about');
+    res.render('about');
 });
 
+// dynamic routes to project pages
+app.get('/project/:id', (req, res, next) => {
+    // check if a project with this id exists
+    if (data[req.params.id]) {
+        // if so, render the page passing data at id's index
+        res.render('project', data.projects[req.params.id])
+    } else {
+        // if not construct new error
+        const err = new Error();
+        // give page not found error status
+        err.status = 404;
+        // give user friendly message
+        err.message = "I'm sorry, this project doesn't seem to exist!";
+        // continue, passing error
+        next(err);
+    }
+});
+
+// 404 handler
+app.use((req, res, next) => {
+    const err = new Error();
+    err.status = 404;
+    err.message = "I'm sorry, this page doesn't seem to exist!";
+    next(err);
+})
+
+// global error handler
+app.use((err, req, res, next) => {
+    if (err.status === 404) {
+        next(err);
+    } else {
+        err.status = 500;
+        err.message = "I'm sorry, something went wrong with the server!"
+        next(err);
+    }
+})
+
+// app will listen on port 3000 and send message to the console
 app.listen(3000, () => {
     console.log('The app is listening on port 3000');
 });
